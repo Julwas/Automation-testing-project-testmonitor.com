b188788a3761d4e68b9abef1ceb6fa1a1f385bea
Julia Wasilewska <131763987+Julwas@users.noreply.github.com>
2023-10-01 01:18:21 +0200

    Update MyProjectsPage.java

--- src/test/java/pages/MyProjectsPage.java
+++ src/test/java/pages/MyProjectsPage.java
@@ -12,7 +12,7 @@ public class MyProjectsPage extends BasePage {
     private final By projectPageLocator = By.xpath("//h1[contains(text(), 'Projects')]");
     private final By manageButtonLocator = By.xpath("//a[contains(text(),'Manage Projects')]");
     private final By createButtonLocator = By.xpath("//button[contains(text(),'Create project...')]");
-    private final By nameInputLocator = By.xpath("//input[@id='field-37']");
+    private final By nameInputLocator = By.xpath("//input[starts-with(@id, 'field')]");
     private final By buttonFeaturesLocator = By.xpath("//span[contains(text(),'Features')]");
     private final By buttonTemplateLocator = By.xpath("//button//span[contains(text(),'Template')]");
     private final By buttonCreateLocator = By.xpath("//span[contains(text(),'Create')]");
